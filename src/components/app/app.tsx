8cc39521bb18737b131a2d3a780380dbc4427df6
vlad-tokarev799 <vlad.tokarev.200247@gmail.com>
2023-02-20 18:01:17 +0800

    first commit

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {
+
+};
+
+export const App = (props: Props) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+import {App} from './components/app/app';
+// import store, { persistor } from './store/store';
+// import NetworkStatus from './components/network-status/network-status';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+    {/*<Provider store={store}>*/}
+    {/*  <PersistGate loading={null} persistor={persistor}>*/}
+    {/*    <NetworkStatus>*/}
+    {/*      <App />*/}
+    {/*    </NetworkStatus>*/}
+    {/*  </PersistGate>*/}
+    {/*</Provider>*/}
+  </React.StrictMode>
+);
